6d3c76b876d12d0be8e1d8545302badf1974e680
saiju <saiju@DESKTOP-EIC10K6>
2023-07-31 12:01:52 +0530

    sample

---
+++ NewProject/src/main/java/Test/Program.java
@@ -0,0 +1,15 @@
+package Test;
+
+public class Program {
+	public void print()
+	{
+		System.out.println("Hello World");
+	}
+
+	public static void main(String[] args) {
+		Program obj=new Program();
+		obj.print();
+
+	}
+
+}
