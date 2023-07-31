6e37c92052dfed6f7647a1b0ba0116198fec0381
saiju <saiju@DESKTOP-EIC10K6>
2023-07-31 12:28:38 +0530

    program

---
+++ example/src/main/java/reenu/Hello.java
@@ -0,0 +1,18 @@
+package reenu;
+
+public class Hello {
+	public void display() {
+	System.out.println("hai");
+	}
+	
+
+	public static void main(String[] args) {
+		Hello obj=new Hello();
+		obj.display();
+		
+		
+	
+
+	}
+
+}
